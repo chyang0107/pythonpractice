129d022c9fd01026a2fdd0181fd66aba90520381
Changheng Yang <chayang@south-murray-south-private-dip136.nat.okstate.edu>
2020-01-21 13:17:02 -0600

    Classes completed by 103

--- udemyList.py
+++ udemyList.py
@@ -1,3 +1,23 @@
 # Date: 1/21/2020
+# Subject: List
+nums = [6, 1, 45, 9, 55]
+# print(len(takes))
+# takes = list(range(1, 4))
+# print(len(takes))
+# for num in nums:
+#     print(num)
 
- 
+# index = 0
+# while index < len(nums):
+#     print(f"{index} is : {nums[index]}")
+#     index += 1
+
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+# Define your code below:
+result = ''
+for sound in sounds:
+    result += sound.upper()
+
+print(result)
+# Class completed: 103
