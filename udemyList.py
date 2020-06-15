a8bc55675a8cf7de686f3d2447b79b4d6ec9c30b
Changheng Yang <chayang@MUR361-CYang.local>
2020-06-15 00:57:08 -0500

    finish the class 148

--- udemyList.py
+++ udemyList.py
@@ -1,64 +1,64 @@
-# Date: 1/21/2020
-# Subject: List
-nums = [6, 1, 45, 9, 55]
-# print(len(takes))
-# takes = list(range(1, 4))
-# print(len(takes))
-# for num in nums:
-#     print(num)
+# # Date: 1/21/2020
+# # Subject: List
+# nums = [6, 1, 45, 9, 55]
+# # print(len(takes))
+# # takes = list(range(1, 4))
+# # print(len(takes))
+# # for num in nums:
+# #     print(num)
 
-# index = 0
-# while index < len(nums):
-#     print(f"{index} is : {nums[index]}")
-#     index += 1
+# # index = 0
+# # while index < len(nums):
+# #     print(f"{index} is : {nums[index]}")
+# #     index += 1
 
-sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+# sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
 
-# Define your code below:
-result = ''
-for sound in sounds:
-    result += sound.upper()
+# # Define your code below:
+# result = ''
+# for sound in sounds:
+#     result += sound.upper()
 
-print(result)
-# Class completed: 103
+# print(result)
+# # Class completed: 103
 
-# Date: 02/04/2020
-# Class : 104  
-# learning the append or extend to add elements in the lust
-# 'Insert'--> add an element in a given position 
-# ends at class 106
+# # Date: 02/04/2020
+# # Class : 104  
+# # learning the append or extend to add elements in the lust
+# # 'Insert'--> add an element in a given position 
+# # ends at class 106
 
-# Date: 2/14/2020
-# ends at class 110
+# # Date: 2/14/2020
+# # ends at class 110
 
-# Date: 2/17/2020
-# !begin at class 111 List Comprehension
+# # Date: 2/17/2020
+# # !begin at class 111 List Comprehension
 
-numbers = [1, 2, 3, 4, 5]
-double_numbers = []
+# numbers = [1, 2, 3, 4, 5]
+# double_numbers = []
 
-for num in numbers:
-    double_number = num * 2
-    double_numbers.append(double_number)
-print(double_numbers)
+# for num in numbers:
+#     double_number = num * 2
+#     double_numbers.append(double_number)
+# print(double_numbers)
 
-numbers = [12, 13, 14, 15, 16]
-doubleNumbers = [num * 2 for num in numbers]
-print(doubleNumbers)
+# numbers = [12, 13, 14, 15, 16]
+# doubleNumbers = [num * 2 for num in numbers]
+# print(doubleNumbers)
 
-name = 'colt'
-newName = [char.upper() for char in name]
-print(newName)
+# name = 'colt'
+# newName = [char.upper() for char in name]
+# print(newName)
 
-friends = ['ashley', 'matt', 'michael']
-friendName = [friend[1].capitalize() for friend in friends]
-print(friendName)
+# friends = ['ashley', 'matt', 'michael']
+# friendName = [friend[1].capitalize() for friend in friends]
+# print(friendName)
 
-# Date: 2/23/2020
-# Class ends at 112, List Comprehension with logic
-numbers = [1, 2, 3, 4, 5, 6]
-result = [num * 2 if num % 2 == 0 else num / 2 for num in numbers]
-print(result)
+# # Date: 2/23/2020
+# # Class ends at 112, List Comprehension with logic
+# numbers = [1, 2, 3, 4, 5, 6]
+# result = [num * 2 if num % 2 == 0 else num / 2 for num in numbers]
+# print(result)
 
 
 # Date: 2/24/2020
@@ -80,3 +80,4 @@ for l in nestedList:
 # Class 141. Iterating Dictionaries finished on 6/9
 # Set and Tuple; set provide the unique dataset
 # finish the class 145
+# Finish the class 148 06/15/2020
